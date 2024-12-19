ad39105235103389929bcacaefa10de5d6d1022d
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-12-19 18:34:01 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -169,94 +169,103 @@ globalThis.bytebeat = new class {
 			}
 		}
 		// Drawing in a segment
-		const { drawMode } = this.settings;
-		const isCombined = drawMode === 'Combined';
-		const isDiagram = drawMode === 'Diagram';
-		const isWaveform = drawMode === 'Waveform';
-		const { colorDiagram } = this;
-		const colorPoints = this.colorWaveform;
-		const colorWaveform = !isWaveform ? colorPoints : [
-			Math.floor(.6 * colorPoints[0] | 0),
-			Math.floor(.6 * colorPoints[1] | 0),
-			Math.floor(.6 * colorPoints[2] | 0)];
-		let ch, drawDiagramPoint, drawPoint, drawWavePoint;
-		for(let i = 0; i < bufferLen; ++i) {
-			const curY = buffer[i].value;
-			const prevY = buffer[i - 1]?.value ?? [NaN, NaN];
-			const isNaNCurY = [isNaN(curY[0]), isNaN(curY[1])];
-			const curTime = buffer[i].t;
-			const nextTime = buffer[i + 1]?.t ?? endTime;
-			const curX = this.mod(Math.floor(this.getX(isReverse ? nextTime + 1 : curTime)) - startX, width);
-			const nextX = this.mod(Math.ceil(this.getX(isReverse ? curTime + 1 : nextTime)) - startX, width);
-			let diagramSize, diagramStart;
-			if(isCombined || isDiagram) {
-				diagramSize = Math.max(1, 256 >> scale);
-				diagramStart = diagramSize * this.mod(curTime, 1 << scale);
-			} else if(isNaNCurY[0] || isNaNCurY[1]) {
-				// Error value - filling with red color
-				for(let x = curX; x !== nextX; x = this.mod(x + 1, width)) {
-					for(let y = 0; y < height; ++y) {
-						const idx = (drawWidth * y + x) << 2;
-						if(!data[idx + 1] && !data[idx + 2]) {
-							data[idx] = 100; // Error: red color
-						}
-					}
-				}
-			}
-			// Select mono or stereo drawing
-			if((curY[0] === curY[1] || isNaNCurY[0] && isNaNCurY[1]) && prevY[0] === prevY[1]) {
-				ch = 1;
-				drawDiagramPoint = isCombined ? this.drawSoftPointMono : this.drawPointMono;
-				drawPoint = this.drawPointMono;
-				drawWavePoint = isCombined ? this.drawPointMono : this.drawSoftPointMono;
-			} else {
-				ch = 2;
-				drawDiagramPoint = isCombined ? this.drawSoftPointStereo : this.drawPointStereo;
-				drawPoint = this.drawPointStereo;
-				drawWavePoint = isCombined ? this.drawPointStereo : this.drawSoftPointStereo;
-			}
-			while(ch--) {
-				const curYCh = curY[ch];
-				const colorCh = this.colorChannels;
-				// Diagram drawing
-				if(isCombined || isDiagram) {
-					const isNaNCurYCh = isNaNCurY[ch];
-					const value = (curYCh & 255) / 256;
-					const color = [
-						value * colorDiagram[0] | 0,
-						value * colorDiagram[1] | 0,
-						value * colorDiagram[2] | 0];
-					for(let x = curX; x !== nextX; x = this.mod(x + 1, width)) {
-						for(let y = 0; y < diagramSize; ++y) {
-							const idx = (drawWidth * (diagramStart + y) + x) << 2;
-							if(isNaNCurYCh) {
-								data[idx] = 100; // Error: red color
-							} else {
-								drawDiagramPoint(data, idx, color, colorCh, ch);
-							}
-						}
-					}
-				}
-				if(isNaNCurY[ch] || isDiagram) {
-					continue;
-				}
-				// Points drawing
-				for(let x = curX; x !== nextX; x = this.mod(x + 1, width)) {
-					drawPoint(data, (drawWidth * (255 - curYCh) + x) << 2, colorPoints, colorCh, ch);
-				}
-				// Waveform vertical lines drawing
-				if(isCombined || isWaveform) {
-					const prevYCh = prevY[ch];
-					if(isNaN(prevYCh)) {
-						continue;
-					}
-					const x = isReverse ? this.mod(Math.floor(this.getX(curTime)) - startX, width) : curX;
-					for(let dy = prevYCh < curYCh ? 1 : -1, y = prevYCh; y !== curYCh; y += dy) {
-						drawWavePoint(data, (drawWidth * (255 - y) + x) << 2, colorWaveform, colorCh, ch);
-					}
-				}
-			}
-		}
+const { drawMode } = this.settings;
+const isCombined = drawMode === 'Combined';
+const isDiagram = drawMode === 'Diagram';
+const isWaveform = drawMode === 'Waveform';
+const isFFT = drawMode === 'FFT';
+const { colorDiagram } = this;
+const colorPoints = this.colorWaveform;
+const colorWaveform = !isWaveform ? colorPoints : [
+    Math.floor(.6 * colorPoints[0] | 0),
+    Math.floor(.6 * colorPoints[1] | 0),
+    Math.floor(.6 * colorPoints[2] | 0)];
+let ch, drawDiagramPoint, drawPoint, drawWavePoint, drawFFTPoint;
+for(let i = 0; i < bufferLen; ++i) {
+    const curY = buffer[i].value;
+    const prevY = buffer[i - 1]?.value ?? [NaN, NaN];
+    const isNaNCurY = [isNaN(curY[0]), isNaN(curY[1])];
+    const curTime = buffer[i].t;
+    const nextTime = buffer[i + 1]?.t ?? endTime;
+    const curX = this.mod(Math.floor(this.getX(isReverse ? nextTime + 1 : curTime)) - startX, width);
+    const nextX = this.mod(Math.ceil(this.getX(isReverse ? curTime + 1 : nextTime)) - startX, width);
+    let diagramSize, diagramStart;
+    if(isCombined || isDiagram) {
+        diagramSize = Math.max(1, 256 >> scale);
+        diagramStart = diagramSize * this.mod(curTime, 1 << scale);
+    } else if(isNaNCurY[0] || isNaNCurY[1]) {
+        // Error value - filling with red color
+        for(let x = curX; x !== nextX; x = this.mod(x + 1, width)) {
+            for(let y = 0; y < height; ++y) {
+                const idx = (drawWidth * y + x) << 2;
+                if(!data[idx + 1] && !data[idx + 2]) {
+                    data[idx] = 100; // Error: red color
+                }
+            }
+        }
+    }
+    // Select mono or stereo drawing
+    if((curY[0] === curY[1] || isNaNCurY[0] && isNaNCurY[1]) && prevY[0] === prevY[1]) {
+        ch = 1;
+        drawDiagramPoint = isCombined ? this.drawSoftPointMono : this.drawPointMono;
+        drawPoint = this.drawPointMono;
+        drawWavePoint = isCombined ? this.drawPointMono : this.drawSoftPointMono;
+        drawFFTPoint = this.drawFFTMono;
+    } else {
+        ch = 2;
+        drawDiagramPoint = isCombined ? this.drawSoftPointStereo : this.drawPointStereo;
+        drawPoint = this.drawPointStereo;
+        drawWavePoint = isCombined ? this.drawPointStereo : this.drawSoftPointStereo;
+        drawFFTPoint = this.drawFFTStereo;
+    }
+    while(ch--) {
+        const curYCh = curY[ch];
+        const colorCh = this.colorChannels;
+        // Diagram drawing
+        if(isCombined || isDiagram) {
+            const isNaNCurYCh = isNaNCurY[ch];
+            const value = (curYCh & 255) / 256;
+            const color = [
+                value * colorDiagram[0] | 0,
+                value * colorDiagram[1] | 0,
+                value * colorDiagram[2] | 0];
+            for(let x = curX; x !== nextX; x = this.mod(x + 1, width)) {
+                for(let y = 0; y < diagramSize; ++y) {
+                    const idx = (drawWidth * (diagramStart + y) + x) << 2;
+                    if(isNaNCurYCh) {
+                        data[idx] = 100; // Error: red color
+                    } else {
+                        drawDiagramPoint(data, idx, color, colorCh, ch);
+                    }
+                }
+            }
+        }
+        if(isNaNCurY[ch] || isDiagram) {
+            continue;
+        }
+        // Points drawing
+        for(let x = curX; x !== nextX; x = this.mod(x + 1, width)) {
+            drawPoint(data, (drawWidth * (255 - curYCh) + x) << 2, colorPoints, colorCh, ch);
+        }
+        // Waveform vertical lines drawing
+        if(isCombined || isWaveform) {
+            const prevYCh = prevY[ch];
+            if(isNaN(prevYCh)) {
+                continue;
+            }
+            const x = isReverse ? this.mod(Math.floor(this.getX(curTime)) - startX, width) : curX;
+            for(let dy = prevYCh < curYCh ? 1 : -1, y = prevYCh; y !== curYCh; y += dy) {
+                drawWavePoint(data, (drawWidth * (255 - y) + x) << 2, colorWaveform, colorCh, ch);
+            }
+        }
+        // FFT drawing
+        if(isFFT) {
+            for(let x = curX; x !== nextX; x = this.mod(x + 1, width)) {
+                drawFFTPoint(data, (drawWidth * (255 - curYCh) + x) << 2, colorPoints, colorCh, ch);
+            }
+        }
+    }
+}
 		// Saving the last points of a segment
 		if(scale) {
 			const x = isReverse ? 0 : drawWidth - 1;
