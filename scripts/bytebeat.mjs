1aaa933aa20c44bad3d5afa188bbe339f6508ba9
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-12-24 11:08:28 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -1202,18 +1202,14 @@ globalThis.bytebeat = new class {
 	toggleTimeCursor() {
 		this.canvasTimeCursor.classList.toggle('hidden', !this.timeCursorEnabled);
 	}
-updateUrl() {
-    const code = this.editorValue;
-    const songData = { code };
-    if (this.songData.sampleRate !== 8000) {
-        songData.sampleRate = this.songData.sampleRate;
-    }
-    if (this.songData.mode !== 'Bytebeat') {
-        songData.mode = this.songData.mode;
-    }
-    this.setCodeSize(code);
-    const newHash = `#GFLJBeat3-${btoa(String.fromCharCode.apply(undefined,
-        deflateRaw(JSON.stringify(songData)))).replaceAll('=', '')}`;
-    history.replaceState(undefined, undefined, newHash);
-}
+	updateLocation() {
+		const pData = { code: this.editorElem.value };
+		if(this.sampleRate !== 8000) {
+			pData.sampleRate = this.sampleRate;
+		}
+		if(this.mode !== 'Bytebeat') {
+			pData.mode = this.mode;
+		}
+		window.location.hash = '#GFLJBeat3-' + btoa(pako.deflateRaw(JSON.stringify(pData), { to: 'string' }));
+	}
 }();
