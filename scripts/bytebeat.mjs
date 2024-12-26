a9ace5c29d82be6b67b8cad0428b34978356655a
gfljs2100-user3 <121681495+gfljs2100-user3@users.noreply.github.com>
2024-12-26 17:11:11 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -1106,25 +1106,25 @@ generateLibraryEntry({
 				sampleRatio: this.songData.sampleRate / this.audioCtx.sampleRate
 			});
 		}
-	}
-	setScale(amount, buttonElem) {
-		if(buttonElem?.getAttribute('disabled')) {
-			return;
-		}
-		const scale = Math.max(this.settings.drawScale + amount, 0);
-		this.settings.drawScale = scale;
-		this.controlScale.innerHTML = !scale ? '1x' :
-			scale < 7 ? `1/${ 2 ** scale }${ scale < 4 ? 'x' : '' }` :
-			`<sub>2</sub>-${ scale }`;
-		this.saveSettings();
-		this.clearCanvas();
-		this.toggleTimeCursor();
-		if(this.settings.drawScale <= 0) {
-			this.controlScaleDown.setAttribute('disabled', true);
-		} else {
-			this.controlScaleDown.removeAttribute('disabled');
-		}
-	}
+	}setScale(amount, buttonElem) {
+    if (buttonElem?.getAttribute('disabled')) {
+        return;
+    }
+    const scale = Math.max(this.settings.drawScale + amount, 0);
+    this.settings.drawScale = scale;
+    this.controlScale.innerHTML = !scale ? '1x' :
+        scale < 7 ? `1/${2 ** scale}${scale < 4 ? 'x' : ''}` :
+            `<sub>2</sub>-${scale}`;
+    this.saveSettings();
+    this.clearCanvas();
+    this.toggleTimeCursor();
+    if (this.settings.drawScale <= 0) {
+        this.controlScaleDown.setAttribute('disabled', true);
+    } else {
+        this.controlScaleDown.removeAttribute('disabled');
+    }
+    this.toggleTimeCursor();
+}
 	setThemeStyle(value) {
 		if(value === undefined) {
 			if((value = this.settings.themeStyle) === undefined) {
@@ -1211,9 +1211,10 @@ generateLibraryEntry({
 		this.audioRecordChunks = [];
 		this.playbackToggle(true);
 	}
-	toggleTimeCursor() {
-		this.canvasTimeCursor.classList.toggle('hidden', !this.timeCursorEnabled);
-	}
+toggleTimeCursor() {
+    const timeCursorEnabled = Math.abs(this.songData.sampleRate * this.playbackSpeed) < 2000;
+    this.canvasTimeCursor.classList.toggle('hidden', !timeCursorEnabled);
+}
 updateUrl() {
     const code = this.editorValue;
     const songData = { code };
