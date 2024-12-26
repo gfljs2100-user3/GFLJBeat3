ecbaeee34489eeb98df077a295624ae1f2c25a86
gfljs2100-user3 <121681495+gfljs2100-user3@users.noreply.github.com>
2024-12-26 17:02:24 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -94,7 +94,7 @@ globalThis.bytebeat = new class {
 		return saveData;
 	}
 	get timeCursorEnabled() {
-	    return Math.abs(this.songData.sampleRate * this.playbackSpeed) < (2000 << this.settings.drawScale);
+	    return Math.abs(this.songData.sampleRate * this.playbackSpeed) < 2000;
 	}
 	animationFrame() {
 		this.drawGraphics(this.byteSample);
