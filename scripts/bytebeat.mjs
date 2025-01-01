7c162f372e069b85cc443139bd27e6e45eed9a35
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-01 18:55:09 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -424,17 +424,17 @@ generateLibraryEntry({
         if(fileFormatted) {
             codeBtn += `<button class="code-button code-load code-load-formatted" data-songdata='${
                 songData }' data-code-file="${ file
-            }" title="Click to load and play the formatted code">formatted</button>`;
+            }" title="Click to load and play the formatted code">formatted ${this.formatBytes(calculatedSize)}</button>`;
         }
         if(fileOriginal) {
             codeBtn += `<button class="code-button code-load code-load-original" data-songdata='${
                 songData }' data-code-file="${ file
-            }" title="Click to load and play the original code">original</button>`;
+            }" title="Click to load and play the original code">original ${this.formatBytes(calculatedSize)}</button>`;
         }
         if(fileMinified) {
             codeBtn += `<button class="code-button code-load code-load-minified" data-songdata='${
                 songData }' data-code-file="${ file
-            }" title="Click to load and play the minified code">minified</button>`;
+            }" title="Click to load and play the minified code">minified ${this.formatBytes(calculatedSize)}</button>`;
         }
         if(codeBtn) {
             entry += `<div class="code-buttons-container">${ codeBtn }</div>`;
