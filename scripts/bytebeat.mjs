ae2e82d0976eb5aae5ce2f376bca886a98860743
gfljs2100-user3 <121681495+gfljs2100-user3@users.noreply.github.com>
2025-01-04 20:12:43 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -467,6 +467,9 @@ generateLibraryEntry({
     // Ensure the <details> tag is properly closed and structured
     if (childrenStr) {
         entry += ` <details><summary>${authorsList ? `by ${authorsList}` : 'Show/Hide songs'}</summary><div class="entry-children">${childrenStr}</div></details>`;
+    } else {
+        // If there's only one song, use the details tag to hide/show it
+        entry += ` <details><summary>${authorsList ? `by ${authorsList}` : 'Show/Hide song'}</summary>${entry}</details>`;
     }
     
     return `<div class="${ codeOriginal || codeMinified || file || children ? 'entry' : 'entry-text' }${
