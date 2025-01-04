590879735f3ebcf32cd3406c513a55f7bcf59eb9
gfljs2100-user3 <121681495+gfljs2100-user3@users.noreply.github.com>
2025-01-04 18:57:01 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -344,7 +344,7 @@ generateLibraryEntry({
         for (let i = 0; i < len; ++i) {
             childrenStr += this.generateLibraryEntry(children[i]);
         }
-        entry += ` <details><summary>by ${authorsList}</summary><div class="entry-children">${childrenStr}</div></details>`;
+        entry += ` <details><summary>Show / Hide songs</summary><div class="entry-children">${childrenStr}</div></details>`;
     }
     if(url && (!noArrayUrl || !name && !author)) {
         if(noArrayUrl) {
