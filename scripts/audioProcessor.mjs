f77d6d262fb1c14db469b7b8ee6e59f3fd8599f1
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-09 16:57:41 +0800

    Update audioProcessor.mjs

--- scripts/audioProcessor.mjs
+++ scripts/audioProcessor.mjs
@@ -326,9 +326,29 @@ class audioProcessor extends AudioWorkletProcessor {
 		this.outValue = [0, 0];
 	}
 	setFunction(codeText) {
+		const chyx = {
+			/*bit*/        "bitC": function (x, y, z) { return x & y ? z : 0 },
+			/*bit reverse*/"br": function (x, size = 8) {
+				if (size > 32) { throw new Error("br() Size cannot be greater than 32") } else {
+					let result = 0;
+					for (let idx = 0; idx < (size - 0); idx++) {
+						result += chyx.bitC(x, 2 ** idx, 2 ** (size - (idx + 1)))
+					}
+					return result
+				}
+			},
+			/*sin that loops every 128 "steps", instead of every pi steps*/"sinf": function (x) { return Math.sin(x / (128 / Math.PI)) },
+			/*cos that loops every 128 "steps", instead of every pi steps*/"cosf": function (x) { return Math.cos(x / (128 / Math.PI)) },
+			/*tan that loops every 128 "steps", instead of every pi steps*/"tanf": function (x) { return Math.tan(x / (128 / Math.PI)) },
+			/*converts t into a string composed of it's bits, regex's that*/"regG": function (t, X) { return X.test(t.toString(2)) }
+			/*corrupt sound"crpt": function(x,y=8) {return chyx.br(chyx.br(x,y)+t,y)^chyx.br(t,y)},
+			decorrupt sound"decrpt": function(x,y=8) {return chyx.br(chyx.br(x^chyx.br(t,y),y)-t,y)},*/
+		}
 		// Create shortened Math functions
 		const params = Object.getOwnPropertyNames(Math);
 		const values = params.map(k => Math[k]);
+		const chyxNames = Object.getOwnPropertyNames(chyx);
+		const chyxFuncs = chyxNames.map(k => chyx[k]);
 		params.push('int', 'window');
 		values.push(Math.floor, globalThis);
 		audioProcessor.deleteGlobals();
