e3e6f7ba0d0bf7817a9889629836b947c40f5052
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-20 18:34:38 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -608,7 +608,7 @@ generateLibraryEntry({
 	    loadScript('./scripts/codemirror.min.mjs?version=2024090100');
 	}
 	async initAudioContext() {
-		this.audioCtx = new AudioContext({ latencyHint: 'balanced', sampleRate: 192000});
+		this.audioCtx = new AudioContext({ latencyHint: 'balanced', sampleRate: 768000});
 		this.audioGain = new GainNode(this.audioCtx);
 		this.audioGain.connect(this.audioCtx.destination);
 		await this.audioCtx.audioWorklet.addModule('./scripts/audioProcessor.mjs?version=2024090100');
