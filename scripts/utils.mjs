6789ddfce4b654f6cc01f6086149598a875a168e
gfljs2100-user3 <121681495+gfljs2100-user3@users.noreply.github.com>
2025-01-20 17:46:10 +0800

    Update utils.mjs

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -1,7 +1,7 @@
-	export formatBytes(bytes) {
+export function formatBytes(bytes) {
 	if(bytes < 1E4) {
 		return bytes + 'B';
 	}
 	const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-	return (i ? (bytes / (1024 ** i)).toFixed(2) : bytes) + ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'][i];
+	return (i ? (bytes / (1024 ** i)).toFixed(2) : bytes) + ['B', 'KB', 'MB', 'GB', 'TB'][i];
 }
