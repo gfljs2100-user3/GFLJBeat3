8d152d5f79c84120b7603bd7cb7ec7ebee1e9357
gfljs2100-user3 <121681495+gfljs2100-user3@users.noreply.github.com>
2025-01-20 17:43:43 +0800

    Create utils.mjs

---
+++ scripts/utils.mjs
@@ -0,0 +1,7 @@
+	export formatBytes(bytes) {
+	if(bytes < 1E4) {
+		return bytes + 'B';
+	}
+	const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+	return (i ? (bytes / (1024 ** i)).toFixed(2) : bytes) + ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'][i];
+}
