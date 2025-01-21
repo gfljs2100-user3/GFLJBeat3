4a107534ce9516a67f358753344727ebbaf8f859
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-21 21:02:24 +0800

    Delete scripts/utils2.mjs

--- scripts/utils2.mjs
+++
@@ -1,7 +0,0 @@
-export function formatBytes2 (bytes, unit) {
-	  if (bytes < (unit = unit || 1000)) 
-	    return bytes + "B";
-		  var exp = parseInt(Math.floor(Math.log(bytes) / Math.log(unit)), 10);
-		  var pre = '' + (unit === 1000 ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (unit === 1000 ? "" : "i") + 'B';
-	    return (bytes / (Math.pow(unit, exp))).toFixed(2) + pre;
-}
