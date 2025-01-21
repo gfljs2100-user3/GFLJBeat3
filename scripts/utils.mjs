dc00215b2d0eff471ce76c6d299c988a2944a500
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-21 18:45:48 +0800

    Update utils.mjs

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -1,9 +1,9 @@
 export function formatBytes (bytes, unit) {
-	const unit1024 = unit || 1024
-	  if (bytes < (unit = unit || 1000)) 
+	const unit1024 = unit || 1E4
+	  if (bytes < (unit = unit || 1E4)) 
 	    return bytes + "B";
 		  var exp = parseInt(Math.floor(Math.log(bytes) / Math.log(unit)), 10);
-		  var pre = '' + (unit === 1000 ? "kMGTPE" : "KMGTPE").charAt(exp) + (unit === 1000 ? "" : "i") + 'B';
-		  var pre1024 = '' + (unit1024 === 1000 ? "kMGTPE" : "KMGTPE").charAt(exp) + (unit1024 === 1000 ? "" : "i") + 'B';
+		  var pre = '' + (unit === 1000 ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (unit === 1000 ? "" : "i") + 'B';
+		  var pre1024 = '' + (unit1024 === 1000 ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (unit1024 === 1000 ? "" : "i") + 'B';
 	    return `${(bytes / (Math.pow(unit, exp))).toFixed(2) + pre} (${(bytes / (Math.pow(unit1024, exp))).toFixed(2) + pre1024})`;
 }
