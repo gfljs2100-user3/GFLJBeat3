a8956a82e95c3e3f60a18ddcb654ce1bcccbd549
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-21 16:28:07 +0800

    Update utils.mjs

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -4,5 +4,5 @@ export function formatBytes(bytes) {
 	}
 	const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
 	const pre = ' ' +(bytes === 1000 ? "kMGTPE" : "KMGTPE").charAt (i - 1) + (bytes === 1000 ? "" : "i") + 'B';
-	return (i ? (bytes / (1024 ** i)).toFixed(1) + pre : bytes) + ['B', 'KB', 'MB', 'GB', 'TB'][i];
+	return (bytes / Math.pow (unit, exp)).toFixed (1) + pre;
 }
