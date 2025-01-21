f7443f4196103b9b423cfbf44e8abef596776ab1
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-21 17:54:25 +0800

    Create utils2.mjs

---
+++ scripts/utils2.mjs
@@ -0,0 +1,7 @@
+export function formatBytes (bytes, unit) {
+	  if (bytes < (unit = unit || 1000)) 
+	    return bytes + "B";
+		  var exp = Math.floor(Math.log(bytes) / Math.log(unit));
+		  var pre = '' + (unit === 1000 ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (unit === 1000 ? "" : "i") + 'B';
+	    return (bytes / (Math.pow(unit, exp))).toFixed(2) + pre;
+}
