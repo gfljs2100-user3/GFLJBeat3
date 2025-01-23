afc1e72316b2a3d11ac4d17b528f4b8b2cad4c60
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-23 18:38:58 +0800

    Update utils.mjs

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -6,7 +6,7 @@ export function formatBytes(bytes) {
 	const i1024 = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
 	const a1000 = (i1000 ? (bytes / (1000 ** i1000)).toFixed(2) : bytes) + ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'][i1000];
 	const a1024 = (i1024 ? (bytes / (1024 ** i1024)).toFixed(2) : bytes) + ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'][i1024];
-	return 0?`${a1000}/(${[a1024]})`:`${a1000} (${[a1024]})`;
+	return 0?`${a1000}/${[a1024]}`:`${a1000} (${[a1024]})`;
 }
 
 export function formatBytes2(bytes) {
