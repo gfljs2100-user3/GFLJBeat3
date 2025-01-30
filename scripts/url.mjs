b51e87a7f24f899c7664ba0e949fed1cb9d4cc6b
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-30 12:20:54 +0800

    Create url.mjs

---
+++ scripts/url.mjs
@@ -0,0 +1,34 @@
+import { deflateRaw, inflateRaw, ungzip } from './pako.esm.min.mjs';
+export function parseUrl() {
+		let { hash } = window.location;
+		if(!hash) {
+			this.updateUrl();
+			({ hash } = window.location);
+		}
+		let songData;
+		if (hash.startsWith('#v3b64') || hash.startsWith('#GFLJBeat3-')) {
+			try {
+				const hashString = hash.startsWith('#GFLJBeat3-') ? atob(hash.slice(11)) : atob(hash.slice(6));
+				const dataBuffer = new Uint8Array(hashString.length);
+				for (const i in hashString) {
+					if (Object.prototype.hasOwnProperty.call(hashString, i)) {
+						dataBuffer[i] = hashString.charCodeAt(i);
+					}
+				}
+				songData = inflateRaw(dataBuffer, { to: 'string' });
+				if (!songData.startsWith('{')) { // XXX: old format
+					songData = { code: songData, sampleRate: 8000, mode: 'Bytebeat' };
+				} else {
+					songData = JSON.parse(songData);
+					if (songData.formula) { // XXX: old format
+						songData.code = songData.formula;
+					}
+				}
+			} catch (err) {
+				console.error(`Couldn't load data from url: ${err}`);
+			}
+		} else {
+			console.error('Couldn\'t load data from url: unrecognized url data');
+		}
+		this.loadCode(songData || { code: this.editorValue }, false);
+	}
