05a16942edc35de5c7f7b0cdb11db88c2a4f0429
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-30 12:28:21 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -1,6 +1,5 @@
 import { deflateRaw, inflateRaw, ungzip } from './pako.esm.min.mjs';
 import { formatBytes, formatBytes2 } from './utils.mjs';
-import { parseUrl } from './url.mjs';
 
 const loadScript = src => new Promise(resolve => {
 	try {
@@ -903,6 +902,40 @@ generateLibraryEntry({
 	requestAnimationFrame() {
 		window.requestAnimationFrame(() => this.animationFrame());
 	}
+	parseUrl() {
+		let { hash } = window.location;
+		if(!hash) {
+			this.updateUrl();
+			({ hash } = window.location);
+		}
+		let songData;
+		if (hash.startsWith('#v3b64') || hash.startsWith('#GFLJBeat3-')) {
+			try {
+				const hashString = hash.startsWith('#GFLJBeat3-') ? atob(hash.slice(11)) : atob(hash.slice(6));
+				const dataBuffer = new Uint8Array(hashString.length);
+				for (const i in hashString) {
+					if (Object.prototype.hasOwnProperty.call(hashString, i)) {
+						dataBuffer[i] = hashString.charCodeAt(i);
+					}
+				}
+				songData = inflateRaw(dataBuffer, { to: 'string' });
+				if (!songData.startsWith('{')) { // XXX: old format
+					songData = { code: songData, sampleRate: 8000, mode: 'Bytebeat' };
+				} else {
+					songData = JSON.parse(songData);
+					if (songData.formula) { // XXX: old format
+						songData.code = songData.formula;
+					}
+				}
+			} catch (err) {
+				console.error(`Couldn't load data from url: ${err}`);
+			}
+		} else {
+			console.error('Couldn\'t load data from url: unrecognized url data');
+		}
+		this.loadCode(songData || { code: this.editorValue }, false);
+	}
+
 	resetTime() {
 		this.isNeedClear = true;
 		this.sendData({ resetTime: true, playbackSpeed: this.playbackSpeed });
