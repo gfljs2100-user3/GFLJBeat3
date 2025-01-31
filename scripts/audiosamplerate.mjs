2e1d04eb1675cb20922fd959972dbda605a83a3a
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-31 15:51:11 +0800

    Update audiosamplerate.mjs

--- scripts/audiosamplerate.mjs
+++ scripts/audiosamplerate.mjs
@@ -1,1 +1,10 @@
-
+export class AudioSampleRate {
+        constructor() {
+          	      this.settingsAudioRate = null;
+		              this.settingsAudioRateApplyButton = null;
+        }
+        initElements() {
+          		    this.settingsAudioRate = document.getElementById('settings-audiorate');
+		              this.settingsAudioRateApplyButton = document.getElementById('settings-audiorate-apply');
+        }
+}
